dde1e5d8dcd12648e7aa97c03fc5ffbe1e264cac
Emilia Groß-Hardt <56568085+emilia-ghdt@users.noreply.github.com>
2024-07-24 04:29:18 +0200

    Initial configuration

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "Nixos config flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {    
+      nixosConfigurations.cassiopeia = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs; };
+        modules = [ 
+          ./hosts/cassiopeia/configuration.nix
+          inputs.home-manager.nixosModules.default
+        ];
+      };
+
+      nixosConfiguration.orion = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./hosts/orion/configuration.nix
+          inputs.home-manager.nixosModules.default
+        ];
+      };
+      
+formatter = system: nixpkgs.${system}.nixpkgs-fmt;
+  
+    };
+}
---
+++ home.nix
@@ -0,0 +1,47 @@
+{ config, pkgs, inputs, ... }:
+
+{
+  imports = [
+    inputs.sops-nix.homeManagerModules.sops
+  ];  
+
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "emilia";
+  home.homeDirectory = "/home/emilia";
+
+  home.packages = with pkgs; [
+    firefox
+    thunderbird
+    vscodium
+    neovim
+    git
+    cmake
+    discord
+    blender
+    kicad
+    spotify
+    obsidian
+    freecad
+    gimp
+    btop
+    tree
+    du-dust
+    ripgrep
+  ];
+
+  home.stateVersion = "23.11"; # Please read the comment before changing.
+
+  home.sessionVariables = {
+    EDITOR = "nvim";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  # Secrets
+  sops.defaultSopsFile = ./secrets.yaml;
+  sops.defaultSopsFormat = "yaml";
+ 
+  sops.age.keyFile = "/home/emilia/.config/sops/age/keys.txt";
+}
