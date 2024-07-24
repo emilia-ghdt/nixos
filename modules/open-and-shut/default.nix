d26d62a4722270638958d28cfcbb734467991a02
Emilia Groß-Hardt <56568085+emilia-ghdt@users.noreply.github.com>
2024-07-24 04:36:29 +0200

    Disabled openshut suspend

--- modules/open-and-shut/default.nix
+++ modules/open-and-shut/default.nix
@@ -43,15 +43,15 @@ in
 
           # If the user closes the lid without opening it again for this duration, then
           # suspend the system
-          suspend_pause=15
+          # suspend_pause=15
 
           closed_at=$(date +%s%3N)
           ${pkgs.busybox}/bin/pkill -f morse_code_open.sh
 
           echo "$closed_at" > /tmp/morse_code_close.timestamp
 
-          sleep "$suspend_pause"
-          systemctl suspend
+          # sleep "$suspend_pause"
+          # systemctl suspend
         '';
         mode = "0755";
       };
