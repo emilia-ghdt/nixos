2bf6410a0648ed2e77a3a44cb1e2676b84bac0fb
Emilia Groß-Hardt <56568085+emilia-ghdt@users.noreply.github.com>
2025-05-30 00:37:47 +0200

    fiddling around with xdg, i have no idea what i'm doing

--- home/modules/xdg.nix
+++ home/modules/xdg.nix
@@ -1,6 +1,11 @@
-{ lib, pkgs, system-config, ... }:
-with lib;
 {
+  lib,
+  pkgs,
+  system-config,
+  config,
+  ...
+}:
+with lib; {
   xdg = {
     mime.enable = true;
     # icons.enable = true;
@@ -9,12 +14,16 @@ with lib;
       enable = true;
       # wlr.enable = true;
       xdgOpenUsePortal = true;
-      configPackages = with pkgs; [ xdg-desktop-portal-gtk ]
-        ++ lib.optionals (system-config.siren.plasma.enable) [ kdePackages.xdg-desktop-portal-kde ]
-        ++ lib.optionals (system-config.siren.hyprland.enable) [ xdg-desktop-portal-hyprland ];
-      extraPortals = with pkgs; [ xdg-desktop-portal-gtk ]
-        ++ lib.optionals (system-config.siren.plasma.enable) [ kdePackages.xdg-desktop-portal-kde ]
-        ++ lib.optionals (system-config.siren.hyprland.enable) [ xdg-desktop-portal-hyprland ];
+      configPackages = with pkgs;
+        [xdg-desktop-portal-gtk]
+        ++ lib.optionals (system-config.siren.plasma.enable) [kdePackages.xdg-desktop-portal-kde]
+        ++ lib.optionals (config.siren.programs.sway.enable) [xdg-desktop-portal-wlr]
+        ++ lib.optionals (system-config.siren.hyprland.enable) [xdg-desktop-portal-hyprland];
+      extraPortals = with pkgs;
+        [xdg-desktop-portal-gtk]
+        ++ lib.optionals (system-config.siren.plasma.enable) [kdePackages.xdg-desktop-portal-kde]
+        ++ lib.optionals (config.siren.programs.sway.enable) [xdg-desktop-portal-wlr]
+        ++ lib.optionals (system-config.siren.hyprland.enable) [xdg-desktop-portal-hyprland];
     };
 
     userDirs = {
@@ -30,16 +39,16 @@ with lib;
       # "application/x-7z-compressed" = [ "org.kde.ark.desktop" ];
       # "application/x-bzip" = [ "org.kde.ark.desktop" ];
       # "application/x-bzip2" = [ "org.kde.ark.desktop" ];
-      "application/x-sh" = [ "codium.desktop" ];
+      "application/x-sh" = ["codium.desktop"];
       # "application/x-tar" = [ "org.kde.ark.desktop" ];
       # "application/zip" = [ "org.kde.ark.desktop" ];
       # "application/pdf" = [ "org.kde.okular.desktop" ];
       # "application/xml" = [ "org.kde.kate.desktop" ];
-      "audio/flac" = [ "vlc.desktop" ];
-      "audio/mpeg" = [ "vlc.desktop" ];
-      "audio/ogg" = [ "vlc.desktop" ];
-      "audio/opus" = [ "vlc.desktop" ];
-      "audio/webm" = [ "vlc.desktop" ];
+      "audio/flac" = ["vlc.desktop"];
+      "audio/mpeg" = ["vlc.desktop"];
+      "audio/ogg" = ["vlc.desktop"];
+      "audio/opus" = ["vlc.desktop"];
+      "audio/webm" = ["vlc.desktop"];
       # "image/bmp" = [ "org.kde.gwenview.desktop" ];
       # "image/gif" = [ "org.kde.gwenview.desktop" ];
       # "image/jpeg" = [ "org.kde.gwenview.desktop" ];
@@ -47,25 +56,25 @@ with lib;
       # "image/png" = [ "org.kde.gwenview.desktop" ];
       # "image/svg+xml" = [ "org.kde.gwenview.desktop" ];
       # "image/webp" = [ "org.kde.gwenview.desktop" ];
-      "text/calendar" = [ "thunderbird.desktop" ];
-      "text/javascript" = [ "codium.desktop" ];
+      "text/calendar" = ["thunderbird.desktop"];
+      "text/javascript" = ["codium.desktop"];
       # "text/plain" = [ "org.kde.kate.desktop" ];
       # "text/xml" = [ "org.kde.kate.desktop" ];
-      "video/mp4" = [ "mpv.desktop" ];
-      "video/mpeg" = [ "mpv.desktop" ];
-      "video/webm" = [ "mpv.desktop" ];
-      "x-scheme-handler/sgnl" = [ "signal-desktop.desktop" ];
-      "x-scheme-handler/signalcaptcha" = [ "signal-desktop.desktop" ];
-      "x-scheme-handler/http" = [ "firefox.desktop" ];
-      "x-scheme-handler/https" = [ "firefox.desktop" ];
-      "text/html" = [ "firefox.desktop" ];
-      "x-scheme-handler/chrome" = [ "firefox.desktop" ];
-      "application/x-extension-htm" = [ "firefox.desktop" ];
-      "application/x-extension-html" = [ "firefox.desktop" ];
-      "application/x-extension-shtml" = [ "firefox.desktop" ];
-      "application/x-extension-xhtml" = [ "firefox.desktop" ];
-      "application/x-extension-xht" = [ "firefox.desktop" ];
-      "application/xhtml+xml" = [ "firefox.desktop" ];
+      "video/mp4" = ["mpv.desktop"];
+      "video/mpeg" = ["mpv.desktop"];
+      "video/webm" = ["mpv.desktop"];
+      "x-scheme-handler/sgnl" = ["signal-desktop.desktop"];
+      "x-scheme-handler/signalcaptcha" = ["signal-desktop.desktop"];
+      "x-scheme-handler/http" = ["firefox.desktop"];
+      "x-scheme-handler/https" = ["firefox.desktop"];
+      "text/html" = ["firefox.desktop"];
+      "x-scheme-handler/chrome" = ["firefox.desktop"];
+      "application/x-extension-htm" = ["firefox.desktop"];
+      "application/x-extension-html" = ["firefox.desktop"];
+      "application/x-extension-shtml" = ["firefox.desktop"];
+      "application/x-extension-xhtml" = ["firefox.desktop"];
+      "application/x-extension-xht" = ["firefox.desktop"];
+      "application/xhtml+xml" = ["firefox.desktop"];
     };
   };
 }
